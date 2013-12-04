e85c929cb00200594cf42b28de83e1898166846a
Pdaa <Bokal8bit@gmail.com>
2013-12-04 02:03:10 +0100

    Added debug mode on f key and background substraction on space kay

--- ofxMSAPhysics-master/example/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ofMain.h"
-#include "testApp.h"
-
-
-//========================================================================
-int main( ){
-	ofSetupOpenGL(1280, 720, OF_WINDOW);			// <-------- setup the GL context
-	ofRunApp(new testApp());
-}
